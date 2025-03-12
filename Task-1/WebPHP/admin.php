93e59497adc28b3b63680b693acfc796c6659600
Toàn Vũ <toanlss2005+github@gmail.com>
2025-03-13 00:08:06 +0700

    Update admin.php

--- Task-1/WebPHP/admin.php
+++ Task-1/WebPHP/admin.php
@@ -5,7 +5,19 @@
     }
 ?>
 
-<h1>Đăng nhập thành công</h1>
-<a href="logout.php">
-    <button type="submit" name="dangxuat">Logout</button>
-</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+</head>
+<body>
+    <h1>Trang chủ</h1>
+    <h3>Mã đáo thành công - Cung hỉ phát tài</h3>
+    <h3>Tiền ra như nước sông đà, tiền vào nhỏ rọt như cà phê phin</h3>
+    <a href="logout.php">
+        <button type="submit" name="dangxuat">Logout</button>
+    </a>
+</body>
+</html>
